395eafd41719d4f57b16fbfe4365b3c79404b494
rosaliework <rosalie.gomez12@gmail.com>
2017-10-16 16:50:14 -0500

    initial commit

--- src/main/java/sum.java
+++ src/main/java/sum.java
@@ -1,14 +1,21 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class HelloWorldServlet extends HttpServlet {
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+@WebServlet(name = "SumServlet", urlPatterns = "/sum")
+public class sum extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html");
+        int a = 0, b = 0;
+        a = Integer.parseInt(request.getParameter("num1"));
+        b = Integer.parseInt(request.getParameter("num2"));
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        out.println("<h3> Sum res: " + (a+b) + "</h3>");
+
         String name = request.getParameter("name");
         if(name != null){
             out.println("<h3> Hello " + name + "</h3>");
